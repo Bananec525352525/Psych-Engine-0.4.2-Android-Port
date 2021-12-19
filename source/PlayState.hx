2dfe59b72b5755c96e782ee4093d068144bcc352
Bananec525352525 <96386759+Bananec525352525@users.noreply.github.com>
2021-12-19 14:08:49 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -900,7 +900,27 @@ class PlayState extends MusicBeatState
 		iconP2.visible = !ClientPrefs.hideHud;
 		add(iconP2);
 		reloadHealthBarColors();
+        
+        paste
+LOGIN SIGN UP
 
+Guest User
+Código Marca de Água Psych Engine
+A GUEST
+DEC 6TH, 2021
+398
+NEVER
+Not a member of Pastebin yet? Sign Up, it unlocks many cool features!
+0.40 KB
+  
+var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by (sunky ports) "), 24);
+        creditTxt.scrollFactor.set();
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.borderColor = FlxColor.BLACK;
+        creditTxt.borderSize = 3;
+        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+        add(creditTxt);
+        
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
